39bbe6703556729432014d46c0f3fe2e6b82691e
jt <jt.11@mail.ru>
2025-08-04 16:33:53 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "LibXray",
+  platforms: [.iOS(.v15), .macOS(.v13)],
+  products: [
+    .library(name: "FubbleTunnel", targets: ["FubbleTunnel"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "FubbleTunnel",
+      url: "https://github.com/PrimeGuardVPN/FubbleTunnel/releases/download/25.4.7/FubbleTunnel.xcframework.zip",
+      checksum: "90adebe5b07987421805222c1d4381d6bef577ffef4bcf757b59efa76f448a63"
+    )
+  ]
+)
